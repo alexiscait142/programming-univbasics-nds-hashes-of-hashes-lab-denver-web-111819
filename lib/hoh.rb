628d8dbfc231905ee6a829c927e5717719bc7370
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-26 00:13:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,14 +44,14 @@ def naming_system
 
   hash = {
     {
-      label: "Kingdom",
-      sub_category: {
-        label: "Phylum",
-          sub_category: {
-            label: "Class",
-              sub_category: {
-                label: "Order",
-                  sub_category: {}
+      :label => "Kingdom",
+      :sub_category => {
+        :label => "Phylum",
+          :sub_category => {
+            :label => "Class",
+              :sub_category => {
+                :label => "Order",
+                  :sub_category => {}
           }
         }
       }
