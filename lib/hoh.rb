43bc96f0f145e02a54290961f3b8c83fd310a825
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-26 00:10:46 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,11 +46,11 @@ def naming_system
     {
       label: "Kingdom",
       sub_category: {
-        label: "Phylum"
+        label: "Phylum",
           sub_category: {
-            label: "Class"
+            label: "Class",
               sub_category: {
-                label: "Order"
+                label: "Order",
                   sub_category: {}
           }
         }
